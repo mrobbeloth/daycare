d9de0ea16aad1db297f5177fe2c1e8f00485b97b
Michael Robbeloth <michael@robbeloth.com>
2021-07-31 23:31:57 -0400

    Establishing a deployment workflow for this mini-project. Adding the initial DDL script.

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,14 @@
 echo "Creating Daycare Database"
 createdb daycare
 
+# Process the DDL script for the daycare project
+# on the daycare database
+psql -f createTables.sql daycare
+
+# Debugging prompt
 read -p "Press any key to resume ..."
 
-# Remvoe daycare database
+# Remove daycare database 
+# Should be placed into separate script at some point
 echo "Removing Daycare Database"
 dropdb daycare

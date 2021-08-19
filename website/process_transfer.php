ea4747e97c0b0670b423114bac18aab28005c9e9
mrobbeloth <michael@robbeloth.com>
2021-08-19 14:10:55 -0400

    Accidently quoted fetch call

--- website/process_transfer.php
+++ website/process_transfer.php
@@ -27,7 +27,7 @@
                 printf("<p>" . $nameSearchQuery . "</p>");
                 if(!($Result = pg_query($daycareDBConnection, $nameSearchQuery))) {
                     printf("<p>There was a result</p><br/>");
-                    $Cursor = pg_fetch_all("$Result");
+                    $Cursor = pg_fetch_all($Result);
                     printf("<p>Going through each row/p><br/>");
                     foreach($Cursor as $Row) {
                         printf("<p>Going through each column/p><br/>");
